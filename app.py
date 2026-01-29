77c9a93305ee5b4f080094be8d7247cd95796e8e
Manasi Pachpor <manasipachpor2002@gmail.com>
2026-01-29 13:06:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,39 +22,8 @@ def load_data():
     df['LoanAmount'] = df['LoanAmount'].fillna(df['LoanAmount'].mean())
     df['Credit_History'] = df['Credit_History'].fillna(df['Credit_History'].mean())
 
-    df['Loan_Status'] = df['Loan_Status'].map({'Y': 1, 'N': 0})
-    return df
+    df['Loan_Status']_]()
 
-df = load_data()
 
-# ----------------------------
-# Session State
-# ----------------------------
-if 'page' not in st.session_state:
-    st.session_state.page = 1
 
-# ----------------------------
-# Prediction Logic
-# ----------------------------
-def predict_loan(ch, income, loan_amt):
-    if ch == 1.0 and income > loan_amt:
-        return "Approved"
-    else:
-        return "Not Approved"
-
-# ============================
-# PAGE 1 – USER INPUT
-# ============================
-if st.session_state.page == 1:
-    st.title("🏦 Loan Application – Step 1")
-    st.subheader("Enter Applicant Details")
-
-    # Image
-    st.image(
-        "https://cdn-icons-png.flaticon.com/512/3135/3135706.png",
-        width=130
-    )
-
-    with st.form("loan_form"):
-        name = st.text_input("Applicant Name")
-        gender = st.selectbox("Gender", df['Gender]()
+   
