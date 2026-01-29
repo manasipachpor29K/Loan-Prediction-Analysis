63332237fa72498e67dd952271afe34f6027bfc3
Manasi Pachpor <manasipachpor2002@gmail.com>
2026-01-29 13:08:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 
+# ----------------------------
+# Page config
+# ----------------------------
 st.set_page_config(page_title="Loan Approval App", layout="wide")
 
 # ----------------------------
@@ -13,17 +16,11 @@ st.set_page_config(page_title="Loan Approval App", layout="wide")
 def load_data():
     df = pd.read_csv("LP_Train.csv")
 
+    # Clean Dependents
     df['Dependents'] = df['Dependents'].replace(r'\+', '', regex=True)
     df['Dependents'] = df['Dependents'].fillna(0).astype(int)
 
+    # Fill missing values
     df['Gender'] = df['Gender'].fillna('Male')
     df['Married'] = df['Married'].fillna('Yes')
-
-    df['LoanAmount'] = df['LoanAmount'].fillna(df['LoanAmount'].mean())
-    df['Credit_History'] = df['Credit_History'].fillna(df['Credit_History'].mean())
-
-    df['Loan_Status']_]()
-
-
-
-   
+    df['Lo]()
